b668ed8e6e0fa049e18b4a0f3538be5f0929a148
Christoph Henrici <chhenrici@gmail.com>
2019-11-06 17:47:07 +0100

    Investigation Groovy / Kotlin Portability

--- testapp-kotlin-pkg/build.gradle.kts
+++ testapp-kotlin-pkg/build.gradle.kts
@@ -2,13 +2,13 @@ import nu.studer.gradle.credentials.domain.CredentialsContainer
 import com.google.common.collect.Lists
 
 plugins {
-  id("com.apgsga.rpm.package") 
-  id("com.apgsga.zip.package") 
-  id("com.apgsga.publish") 
-  id("org.hidetake.ssh") version "2.10.1"
-  id("nu.studer.credentials")  version "1.0.7"
+	id("com.apgsga.rpm.package")
+	id("com.apgsga.zip.package")
+	id("com.apgsga.publish")
+	id("org.hidetake.ssh") version "2.10.1"
+	id("nu.studer.credentials")  version "1.0.7"
 }
-// Credetilas
+// Credentials
 
 val credentials: CredentialsContainer by project.extra
 val defaultUsername = credentials.getProperty("deployUser") as String
@@ -17,13 +17,13 @@ val defaultPassword = credentials.getProperty("deployUserPassword") as String
 // Command Line Parameter and Defaults
 // TODO (che, 6.11 ): Factor out Commandline Handling in Plugin
 val parServiceName = "echoservice"
-val parTargetHost =  (fun(): String? { return (if (project.hasProperty("targetHost")) project.property("targetHost") else "jadas-e.apgsga.ch") as String? }).invoke()
-val parSshUser = (fun(): String? { return (if (project.hasProperty("sshUser")) project.property("sshUser") else defaultUsername) as String? }).invoke()
-val parSshPw = (fun(): String? { return (if (project.hasProperty("sshPw")) project.property("sshPw") else defaultPassword) as String? }).invoke()
-val parInstallTarget = (fun(): String? { return (if (project.hasProperty("installTarget")) project.property("installTarget") else "CHEI212") as String? }).invoke()
-val parServiceVersion = (fun(): String? { return (if (project.hasProperty("serviceVersion")) project.property("serviceVersion") else "1.0") as String? }).invoke()
-val parReleaseNr = (fun(): String? { return (if (project.hasProperty("releaseNr")) project.property("releaseNr") else "1") as String? }).invoke()
-val parDownloadDir = (fun(): String? { return (if (project.hasProperty("downloadDir")) project.property("downloadDir") else "downloads") as String? }).invoke()
+val parTargetHost =  if (project.hasProperty("targetHost")) project.property("targetHost").toString() else "jadas-e.apgsga.ch"
+val parSshUser =  if (project.hasProperty("sshUser")) project.property("sshUser").toString() else defaultUsername
+val parSshPw =  if (project.hasProperty("sshPw")) project.property("sshPw").toString() else defaultPassword
+val parInstallTarget = if (project.hasProperty("installTarget")) project.property("installTarget").toString() else "CHEI212"
+val parServiceVersion = if (project.hasProperty("serviceVersion")) project.property("serviceVersion").toString() else "1.0"
+val parReleaseNr = if (project.hasProperty("releaseNr")) project.property("releaseNr").toString() else "1"
+val parDownloadDir = if (project.hasProperty("downloadDir")) project.property("downloadDir").toString() else "downloads"
 
 
 println ("Build script using the following parameters:")
