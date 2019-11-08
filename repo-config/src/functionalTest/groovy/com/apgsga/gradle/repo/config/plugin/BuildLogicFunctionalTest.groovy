575162c073830fd9ba6df38b635c5cea87310b53
apgsga-jhe <Julien.Helbling@apgsga.ch>
2019-11-08 08:31:42 +0100

    First changes for IT-34900

--- repo-config/src/functionalTest/groovy/com/apgsga/gradle/repo/config/plugin/BuildLogicFunctionalTest.groovy
+++ repo-config/src/functionalTest/groovy/com/apgsga/gradle/repo/config/plugin/BuildLogicFunctionalTest.groovy
@@ -12,7 +12,7 @@ import spock.lang.Specification
 
 class BuildLogicFunctionalTest extends AbstractSpecification {
 	
-	def "Repo Config works with all repositiories configured"() {
+	def "Repo Config works with all default repositiories configured"() {
 		given:
 			buildFile << """
 	            plugins {
@@ -118,5 +118,53 @@ class BuildLogicFunctionalTest extends AbstractSpecification {
 				
 		then:
 			println "Result output: ${result.output}"
+	}
+	
+	def "Repo Config works with repositories name coming from map"() {
+		
+		// TODO JHE: Well, not sure I got it correctly, how can we configure the artifactory Repo differently within the same build?
+		//			 I would say:
+		//				- As shown below
+		//				- Eventually also in a "publish" section?
+		
+		given:
+			buildFile << """
+	            plugins {
+	                id 'com.apgsga.gradle.repo.config'
+	            }
+
+				apgArtifactoryRepo {
+					repoName = getAvailableRepoNames().get('rpm-publish')
+				}
+
+				apgRepository {
+					artifactory()
+				}
+
+				task listrepos {
+				    doLast {
+				        println "Repositories:"
+				        project.repositories.each { repo -> 
+							println "Repo Name: " + repo.name
+						    println "Repo Url: " + repo.url 
+					    }
+						println "Done"
+				   }
+				}
+	        """
+		when:
+			def result = GradleRunner.create()
+				.withProjectDir(testProjectDir)
+				.withArguments( 'listrepos')
+				.withPluginClasspath()
+				.build()
+				
+		then:
+			println "Result output: ${result.output}"
+			!result.output.contains('MavenLocal')
+			!result.output.contains('MavenLocal2')
+			!result.output.contains('rpm-functionaltest')
+			!result.output.contains('MavenRepo')
+			result.output.contains('yumpatchrepo')
 	}
 }
