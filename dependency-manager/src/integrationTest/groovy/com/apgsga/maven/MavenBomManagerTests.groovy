7ad5fea58c8d356b30b46c7904ca11f4c37a7d5e
Christoph Henrici <chhenrici@gmail.com>
2019-11-14 19:58:43 +0100

    Refactored RepositoryBuilderFactory to accomodate additional usage scenarios

--- dependency-manager/src/integrationTest/groovy/com/apgsga/maven/MavenBomManagerTests.groovy
+++ dependency-manager/src/integrationTest/groovy/com/apgsga/maven/MavenBomManagerTests.groovy
@@ -9,22 +9,29 @@ class MavenBomManagerTests extends Specification {
 
 	private static final String TEST_REPO = "bom-test"
 
-	private static final String BOM_NAME = "test-bom-pom.xml"
+	private static final String BOM_NAME = "test-simple-bom-pom.xml"
 
-	def setup() {
-		def source = new File("src/integrationTest/resources/$BOM_NAME")
-		def destination = new File("$REPO_URL/$TEST_REPO/$BOM_NAME")
-		GFileUtils.copyFile(source,destination)
-	}
-
-	def "load Bom from Repository"() {
+	def "load simple Bom from Repository"() {
 		given:
+			def BOM_NAME = "test-simple-bom-pom.xml"
+			def source = new File("src/integrationTest/resources/$BOM_NAME")
+			def destination = new File("$REPO_URL/$TEST_REPO/$BOM_NAME")
+			GFileUtils.copyFile(source,destination)
 			def bomManager = new MavenBomManagerDefaultImpl(REPO_URL,TEST_REPO,null,null)
 		when:
 			def result = bomManager.loadModel(BOM_NAME)
 		then:
 			result != null
-			println (result.toString())
+			result.size() == 4
+			def expectedArtifacts = ['org.apache.httpcomponents:httpclient:4.5.2:jar','com.google.code.guice:guice:4.1:jar','com.affichage.ui.utils:jgoodies-utils:1.9.6:jar','com.affichage.it21.ppix:ppix-dao:9.0.6.ADMIN-UIMIG-SNAPSHOT:jar']
+			result.each {
+				println(it.toString())
+				def artString = "${it.groupId}:${it.artifactid}:${it.version}:${it.type}".toString()
+				println artString
+				expectedArtifacts.remove(artString)
+			}
+			expectedArtifacts.size() == 0
+
 	}
 	
 	
