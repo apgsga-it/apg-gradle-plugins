b686633a145cabc3ad084ef2551a333312146376
Christoph Henrici <chhenrici@gmail.com>
2019-11-18 18:59:35 +0100

    Kotlin Test Spring Boot Service

---
+++ testapp-kotlin-service/src/main/kotlin/com/apgsga/testapp/TestappApplication.kt
@@ -0,0 +1,40 @@
+package com.apgsga.testapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@SpringBootApplication
+class TestappApplication
+fun main(args: Array<String>) {
+    runApplication<TestappApplication>(*args)
+}
+
+data class EchoMessage (val message: String)
+
+@RestController
+@RequestMapping(path = ["service"])
+class EchoController {
+
+    private val atomicCounter = AtomicLong()
+
+    @RequestMapping(value = ["/echo"])
+    fun echo(@RequestParam(value = "text", defaultValue = "Hello World") text: String): String {
+        return getMessage(text).message
+    }
+
+    @RequestMapping(value = ["/echo/json"], method = [RequestMethod.GET], produces = ["application/json"])
+    fun echoJson(@RequestParam(value = "text", defaultValue = "Hello World") text: String): EchoMessage {
+        return getMessage(text)
+    }
+
+    private fun getMessage(text: String): EchoMessage {
+        return EchoMessage(
+                String.format("%d: %s", atomicCounter.incrementAndGet(), text))
+    }
+
+}
---
+++ testapp-kotlin-service/src/test/kotlin/com/apgsga/testapp/TestappApplicationTest.kt
@@ -0,0 +1,15 @@
+package com.apgsga.testapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestappApplicationTest {
+
+    @Test
+    fun contextLoads() {
+    }
+
+
+
+}
