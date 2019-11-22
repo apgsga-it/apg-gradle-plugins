c4c5c8fe949c50ebe9ddafa48f12ae3f8779d5dc
Christoph Henrici <chhenrici@gmail.com>
2019-11-22 17:18:31 +0100

    Domain level logging

--- dependency-manager/src/integrationTest/groovy/com/apgsga/maven/MavenBomManagerTests.groovy
+++ dependency-manager/src/integrationTest/groovy/com/apgsga/maven/MavenBomManagerTests.groovy
@@ -5,34 +5,69 @@ import spock.lang.Specification
 
 class MavenBomManagerTests extends Specification {
 
-	private static final String REPO_URL = "build/repo"
+    private static final String REPO_URL = "build/repo"
 
-	private static final String TEST_REPO = "bom-test"
+    private static final String TEST_REPO = "bom-test"
 
-	private static final String BOM_NAME = "test-simple-bom-pom.xml"
+    def setup() {
+        def source = new File("src/integrationTest/resources/bomtests")
+        def destination = new File("$REPO_URL/$TEST_REPO")
+        GFileUtils.copyDirectory(source, destination)
+    }
 
-	def "load simple Bom from Repository"() {
+    def "load simple Bom from Repository"() {
+        given:
+
+        def bomManager = new MavenBomManagerDefaultImpl(REPO_URL, TEST_REPO, null, null)
+        when:
+        def result = bomManager.loadModel("test", "test-bom", "1.0")
+        then:
+        result != null
+        result.size() == 4
+        def expectedArtifacts = ['org.apache.httpcomponents:httpclient:4.5.2:jar', 'com.google.code.guice:guice:4.1:jar', 'com.affichage.ui.utils:jgoodies-utils:1.9.6:jar', 'com.affichage.it21.ppix:ppix-dao:9.0.6.ADMIN-UIMIG-SNAPSHOT:jar']
+        result.each {
+            def artString = "${it.groupId}:${it.artifactid}:${it.version}:${it.type}".toString()
+            expectedArtifacts.remove(artString)
+        }
+        expectedArtifacts.size() == 0
+
+    }
+
+    def "load nested Bom from Repository"() {
+        given:
+        def bomManager = new MavenBomManagerDefaultImpl(REPO_URL, TEST_REPO, null, null)
+        when:
+        def result = bomManager.loadModel("test", "test-nested-bom", "1.1")
+        then:
+        result != null
+        result.size() == 4
+        def expectedArtifacts = ['org.apache.httpcomponents:httpclient:4.5.2:jar', 'com.google.code.guice:guice:4.1:jar', 'com.affichage.ui.utils:jgoodies-utils:1.9.6:jar', 'com.affichage.it21.ppix:ppix-dao:9.0.6.ADMIN-UIMIG-SNAPSHOT:jar']
+        result.each {
+            def artString = "${it.groupId}:${it.artifactid}:${it.version}:${it.type}".toString()
+            expectedArtifacts.remove(artString)
+        }
+        expectedArtifacts.size() == 0
+
+    }
+
+	def "load nested Bom with some more artifacts from Repository"() {
 		given:
-			def BOM_NAME = "test-simple-bom-pom.xml"
-			def source = new File("src/integrationTest/resources/bomfiles/$BOM_NAME")
-			def destination = new File("$REPO_URL/$TEST_REPO/$BOM_NAME")
-			GFileUtils.copyFile(source,destination)
-			def bomManager = new MavenBomManagerDefaultImpl(REPO_URL,TEST_REPO,null,null)
+		def bomManager = new MavenBomManagerDefaultImpl(REPO_URL, TEST_REPO, null, null)
 		when:
-			def result = bomManager.loadModel(BOM_NAME)
+		def result = bomManager.loadModel("test", "test-nested-withadd-bom", "1.2")
 		then:
-			result != null
-			result.size() == 4
-			def expectedArtifacts = ['org.apache.httpcomponents:httpclient:4.5.2:jar','com.google.code.guice:guice:4.1:jar','com.affichage.ui.utils:jgoodies-utils:1.9.6:jar','com.affichage.it21.ppix:ppix-dao:9.0.6.ADMIN-UIMIG-SNAPSHOT:jar']
-			result.each {
-				def artString = "${it.groupId}:${it.artifactid}:${it.version}:${it.type}".toString()
-				expectedArtifacts.remove(artString)
-			}
-			expectedArtifacts.size() == 0
+		result != null
+		result.size() == 5
+		def expectedArtifacts = ['org.test:jamesbond:0.0.7:jar','org.apache.httpcomponents:httpclient:4.5.2:jar', 'com.google.code.guice:guice:4.1:jar', 'com.affichage.ui.utils:jgoodies-utils:1.9.6:jar', 'com.affichage.it21.ppix:ppix-dao:9.0.6.ADMIN-UIMIG-SNAPSHOT:jar']
+		result.each {
+			def artString = "${it.groupId}:${it.artifactid}:${it.version}:${it.type}".toString()
+			expectedArtifacts.remove(artString)
+		}
+		expectedArtifacts.size() == 0
 
 	}
-	
-	
+
+
 
 
 }
--- dependency-manager/src/main/kotlin/com/apgsga/maven/MavenBomManagerDefaultImpl.kt
+++ dependency-manager/src/main/kotlin/com/apgsga/maven/MavenBomManagerDefaultImpl.kt
@@ -2,6 +2,7 @@ package com.apgsga.maven
 
 import com.apgsga.gradle.repository.Repository
 import com.apgsga.gradle.repository.RepositoryBuilderFactory
+import org.apache.maven.model.Dependency
 import org.apache.maven.model.Model
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader
 import java.io.InputStream
@@ -19,33 +20,51 @@ class MavenBomManagerDefaultImpl(repoPathBom: String, repoName: String, repoUser
             version
     }
 
-    private fun loadModel(bomFile: InputStream): Collection<MavenArtifact> {
+
+    private fun loadModel(bomFile: InputStream, artList: Collection<MavenArtifact>): Collection<MavenArtifact> {
         logger.lifecycle("Loading Maven Model")
         val mavenReader = MavenXpp3Reader()
         val mavenModel = mavenReader.read(bomFile)
         val dependencies = mavenModel.dependencyManagement.dependencies
-        var artificatList = emptyList<MavenArtifact>()
+        val artifactList = list(dependencies, mavenModel, artList)
+        logger.lifecycle("Loading Maven Model done.")
+        logger.debug("Resolved the following artifactfs: $artifactList")
+        return artifactList
+    }
+
+    private fun list(dependencies: MutableList<Dependency>, mavenModel: Model, artList: Collection <MavenArtifact>) : Collection<MavenArtifact> {
+        var artifactList = artList
         for (dependency in dependencies) {
             val resolvedVersion = resolveVersion(mavenModel, dependency.version)
-            if (resolvedVersion != null) {
-                artificatList = artificatList + MavenArtifact(dependency.groupId, dependency.artifactId, resolvedVersion, dependency.type)
-            } else {
-                logger.error("No artifact found for groupid:  $dependency.groupId, artifactid: $dependency.artifactId, version: $resolvedVersion")
-            }
+            if (resolvedVersion != null && dependency.type == "pom") {
+                // recursively resolve bom
+                artifactList = loadModel(buildPath(dependency.groupId, dependency.artifactId, dependency.version), artifactList)
+            } else if (resolvedVersion != null) {
+                artifactList += MavenArtifact(dependency.groupId, dependency.artifactId, resolvedVersion, dependency.type)
+                } else {
+                    logger.error("No artifact found for groupid:  $dependency.groupId, artifactid: $dependency.artifactId, version: $resolvedVersion")
+                }
 
         }
-        logger.lifecycle("Loading Maven Model done.")
-        logger.debug("Resolved the following artifactfs: $artificatList")
-        return artificatList
+        return artifactList
+    }
+
+    private fun buildPath(groupId: String, artifactid: String, version: String): String {
+        return "$groupId/$artifactid/$version/$artifactid-$version.pom.xml"
+    }
+
+    private fun loadModel(repoPathBom: String, artList: Collection<MavenArtifact>): Collection<MavenArtifact> {
+        val stream = repository.download(repoPathBom)
+        return loadModel(stream, artList)
     }
 
     override fun loadModel(repoPathBom: String): Collection<MavenArtifact> {
-        val stream = repository.download(repoPathBom)
-        return loadModel(stream)
+        val artifactList = emptyList<MavenArtifact>()
+        return loadModel(repoPathBom, artifactList)
     }
 
 
     override fun loadModel(groupId: String, artifactid: String, version: String): Collection<MavenArtifact> {
-        return loadModel("$groupId/$artifactid/$version/$artifactid-$version.pom.xml")
+        return loadModel(buildPath(groupId, artifactid, version))
     }
 }
