fb615f7bbbf488b37253c9d70406d1f349f573bd
Christoph Henrici <chhenrici@gmail.com>
2019-12-18 09:50:47 +0100

    versionResolution Dsl More Testcases

--- dependency-manager/src/functionalTest/groovy/com/apgsga/gradle/maven/dm/VersionResolutionFunctionalTests.groovy
+++ dependency-manager/src/functionalTest/groovy/com/apgsga/gradle/maven/dm/VersionResolutionFunctionalTests.groovy
@@ -13,13 +13,13 @@ class VersionResolutionFunctionalTests extends AbstractSpecification {
             }
             versionResolver {
                  poms {
-                   bom {
+                   aBom {
                         order = 1
                         artifact = "someGroupid:aArtifactid:aBomVersion"
                     }
                  }
                  patches {
-                   patch {
+                   somePatch {
                         order = 2
                         parentDirName = "aParentDir"
                         fileName = "Patch9999.json"
@@ -34,8 +34,61 @@ class VersionResolutionFunctionalTests extends AbstractSpecification {
         println "Result output: ${result.output}"
         result.output.contains('')
     }
-	
-	
+
+
+    def "version Resolution DSL with Version Parameter and single Bom works"() {
+        given:
+        buildFile << """
+            plugins {
+                id("com.apgsga.version.resolver")
+            }
+            project.ext {
+              bomVersion = project.hasProperty('bomVersion') ? project.property('bomVersion') : "XXXXXXXX"
+            }
+            versionResolver {
+                 poms {
+                   che211Bom {
+                        artifact = "someGroupid:aArtifactid:" + bomVersion
+                    }
+                 }
+            }
+        """
+
+        when:
+        def result = gradleRunnerFactory(['init','-PbomVersion=1234']).build()
+        then:
+        println "Result output: ${result.output}"
+        result.output.contains('')
+    }
+
+
+    def "version Resolution DSL with default Version Parameter and single Bom works"() {
+        given:
+        buildFile << """
+            plugins {
+                id("com.apgsga.version.resolver")
+            }
+            project.ext {
+              bomVersion = project.hasProperty('bomVersion') ? project.property('bomVersion') : "XXXXXXXX"
+            }
+            versionResolver {
+                 poms {
+                   che211Bom {
+                        artifact = "someGroupid:aArtifactid:" + bomVersion
+                    }
+                 }
+            }
+        """
+
+        when:
+        def result = gradleRunnerFactory(['init']).build()
+        then:
+        println "Result output: ${result.output}"
+        result.output.contains('')
+    }
+
+
+
 
 }
 
