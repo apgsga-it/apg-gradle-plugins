6058ab1e0f14bba3919375ac99741fc73b049c1f
apgsga-jhe <Julien.Helbling@apgsga.ch>
2019-12-20 08:48:07 +0100

    constant renamed

--- ssh-wrapper/src/main/java/com/apgsga/gradle/ssh/plugin/ApgSshPlugin.java
+++ ssh-wrapper/src/main/java/com/apgsga/gradle/ssh/plugin/ApgSshPlugin.java
@@ -5,21 +5,20 @@ import com.apgsga.gradle.ssh.tasks.DeployRpm;
 import org.gradle.api.NonNullApi;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.plugins.PluginContainer;
 import org.gradle.api.tasks.TaskContainer;
 import org.gradle.api.tasks.TaskProvider;
 
 @NonNullApi
 public class ApgSshPlugin implements Plugin<Project> {
 
-    public static final String APG_SSH_PLUGIN_NAME = "apgSsh";
+    public static final String APG_SSH_EXTENSION_NAME = "apgSsh";
 
     private Project project;
 
     @Override
     public void apply(Project project) {
         this.project = project;
-        project.getExtensions().create(APG_SSH_PLUGIN_NAME, ApgSsh.class, project);
+        project.getExtensions().create(APG_SSH_EXTENSION_NAME, ApgSsh.class, project);
         project.getPlugins().apply("org.hidetake.ssh");
         TaskContainer tasks = project.getTasks();
         TaskProvider<DeployRpm> deployRpmTask = tasks.register(DeployRpm.DEPLOY_RPM_TASK_NAME,DeployRpm.class);
