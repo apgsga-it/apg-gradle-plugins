cc185e089c97f1cd011ce332f2f02ee84461a87e
Christoph Henrici <chhenrici@gmail.com>
2020-01-17 11:08:09 +0100

    First Maven Test Project Structure

--- integration/testapp-service/src/test/java/com/apgsga/testapp/TestappApplicationTests.java
+++ integration/testapp-service/src/test/java/com/apgsga/testapp/TestappApplicationTests.java
@@ -1,14 +1,14 @@
 package com.apgsga.testapp;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
 @SpringBootTest
 public class TestappApplicationTests {
-
+	@DisplayName("Context Load works ok")
 	@Test
 	public void contextLoads() {
 	}
