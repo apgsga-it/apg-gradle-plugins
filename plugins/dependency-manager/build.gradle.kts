a245b29bb01a781f03091f9a751b86b51f50fde1
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-02-11 14:54:03 +0100

    using configuration from init.d

--- plugins/dependency-manager/build.gradle.kts
+++ plugins/dependency-manager/build.gradle.kts
@@ -1,9 +1,3 @@
-apply {
-    from ("${projectDir}/../gradle-common/common-plugin.gradle")
-    from ("${projectDir}/../gradle-common/integration-test.gradle")
-    from ("${projectDir}/../gradle-common/functional-test.gradle")
-}
-
 plugins {
     `kotlin-dsl`
 }
