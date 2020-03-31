3f13f75997d905290095cb878ae5c6b94fdf5d80
Christoph Henrici <chhenrici@gmail.com>
2020-03-31 12:39:58 +0200

    Digiflex Gradle init ok

--- integration/digiflex/digiflex-web-sa-pkg/buildPackageAndDeploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./gradlew clean buildRpm --info -PinstallTarget=CHEI212  -PserviceVersion=1.0 -PreleaseNr=1 -PtargetHost=172.16.92.139 -PsshUser=che
-
- 
-      
--- integration/modules/testapp-pkg/deployRpm.sh
+++ integration/modules/testapp-pkg/deployRpm.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-./gradlew installRpm  -PinstallTarget=CHEI212 -PbaseVersion=1.0 -PbomLastRevision=SNAPSHOT -PrpmReleaseNr=5 -PtargetHost=172.16.92.139 -PsshUser=che -PsshPw=chePw  --stacktrace --info
-
+./gradlew deployRpm  -PinstallTarget=CHEI212 -PbaseVersion=1.0 -PbomLastRevision=SNAPSHOT -PrpmReleaseNr=3 -PtargetHost=192.168.1.28 -PsshUser=che -PsshPw=chePw  --stacktrace --info
