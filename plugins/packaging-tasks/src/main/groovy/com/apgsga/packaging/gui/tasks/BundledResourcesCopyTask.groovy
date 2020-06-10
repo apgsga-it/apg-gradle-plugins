796a5393ef941292e6d164446bcef57a13381047
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-06-10 11:23:44 +0200

    expanding properties for AdGIS.properties as well

--- plugins/packaging-tasks/src/main/groovy/com/apgsga/packaging/gui/tasks/BundledResourcesCopyTask.groovy
+++ plugins/packaging-tasks/src/main/groovy/com/apgsga/packaging/gui/tasks/BundledResourcesCopyTask.groovy
@@ -20,21 +20,25 @@ class BundledResourcesCopyTask extends DefaultTask {
 		ApgCommonPackageExtension ex = project.extensions.apgPackage
 		def digiflexWebIt21PortNr = ex.getPortNr('digiflex-web-it21')
 		def jadasPortNr = ex.getPortNr('jadas')
+		// JHE (10.06.2020): URL such http://chtadg1.apgsga.ch/geoserver/wms are either with "cht" of "chp", basically never starts with "che"
+		def targetIndicatorProdOrOther = ex.installTarget.charAt(2).toLowerCase().equals("p") ? "p" : "t"
 		project.copy {
 			into "${project.buildDir}/${ex.name}"
 			from ("${project.buildDir}/packaging/it21-gui") {
 				expand(javaOpts:ex.javaOpts,mainProgramm:ex.mainProgramName,pkgName:ex.name,digiflexWebIt21PortNr:"${digiflexWebIt21PortNr}",jadasPortNr:"${jadasPortNr}",
-					   target:ex.installTarget.toLowerCase(),targetIndicator:ex.installTarget.charAt(2).toLowerCase())
+					   target:ex.installTarget.toLowerCase(),targetIndicator:ex.installTarget.charAt(2).toLowerCase(),targetIndicatorProdOrOther:"${targetIndicatorProdOrOther}")
 				include '**/*.bat'
 				include '**/*.sh'
 				include '**/logback.xml'
 				include '**/serviceurl.properties'
+				include '**/AdGIS.properties'
 			}
 			from ("${project.buildDir}/packaging/it21-gui") {
 				exclude '**/*.bat'
 				exclude '**/*.sh'
 				exclude '**/logback.xml'
 				exclude '**/serviceurl.properties'
+				exclude '**/AdGIS.properties'
 			}
 	
 		 
