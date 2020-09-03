298c35396ce76c3c18a09a447e822631d5de452b
Christoph Henrici <chhenrici@gmail.com>
2020-09-03 10:39:33 +0200

    Deleted Integration Folder

--- integration/modules/testapp-pkg/deployRpm.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./gradlew deployRpm  -PinstallTarget=CHEI212  -PbomLastRevision=SNAPSHOT -PrpmReleaseNr=1 -PtargetHost=dev-jhedocker.light.apgsga.ch  --stacktrace --info
--- integration/modules/testapp-pkg/installRpm.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./gradlew installRpm  -PinstallTarget=CHEI212  -PbomLastRevision=SNAPSHOT -PrpmReleaseNr=1 -PtargetHost=dev-jhedocker.light.apgsga.ch  --stacktrace --info
