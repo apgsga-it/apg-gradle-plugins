41abf6dee0745997164a95d99be6686d47a8be58
Christoph Henrici <chhenrici@gmail.com>
2021-06-09 08:24:56 +0200

    Bom Depencency Management Analyzer

--- plugins/version-manager/src/main/kotlin/com/apgsga/maven/dm/plugin/BomAnalyzerTask.kt
+++ plugins/version-manager/src/main/kotlin/com/apgsga/maven/dm/plugin/BomAnalyzerTask.kt
@@ -8,7 +8,6 @@ import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
-import kotlin.time.measureTimedValue
 
 abstract class BomAnalyzerTask : DefaultTask() {
 
@@ -27,21 +26,31 @@ abstract class BomAnalyzerTask : DefaultTask() {
     @TaskAction
     fun analyzeBoms() {
         println("Analyzing the following Boms: ")
-        val dependenciesMap = mutableMapOf<String, Collection<Dependency>>()
+        val bomDependenciesMap = mutableMapOf<String, Collection<Dependency>>()
         val mavenBomManagerDefault = MavenBomManagerDefault(GradleDependencyBomLoader(project))
         bomCoordinates.get().forEach {
             println("Retrieving Dependencies for Bom Maven Coordinates: $it")
             val dependencies = mavenBomManagerDefault.retrieve(it, recursive.get())
-            dependenciesMap[it] = dependencies
+            bomDependenciesMap[it] = dependencies
         }
-        println("Dumping all dependencies")
-        // TODO (che, 8.6) : Report Intersection of all Dependency Management Collections
-        // TODO (che, 8.6) : Report redundancy
         // TODO (che, 8.6) : Report Version conflicts
-        dependenciesMap.forEach{(key, dependencies) ->
+        val depencenciesInBomsMap = mutableMapOf<String, MutableCollection<String>>()
+        bomDependenciesMap.forEach{ (key, dependencies) ->
             println ("***** All Bom: $key Dependencies in Dependency Management: ")
             dependencies.forEach {
                 println("Bom : $key, has: ${it.toString()}")
+                val bomCoordinate = "${it.groupId}:${it.artifactId}:${it.version}"
+                if (depencenciesInBomsMap.containsKey(bomCoordinate)){
+                    depencenciesInBomsMap.getValue(bomCoordinate).add(key)
+                } else {
+                    depencenciesInBomsMap.put(bomCoordinate, mutableListOf(key))
+                }
+            }
+        }
+        println{"Report Redundancies:"}
+        depencenciesInBomsMap.forEach { (dependency, bomCoordinates) ->
+            if (bomCoordinates.size > 1) {
+                println("Dependency : <$dependency> in multiple boms : ${bomCoordinates.toString()}  ")
             }
         }
         println("Done.")
